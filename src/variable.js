3d80b74679f6cbb72b6f1cb0ec751e701be5dcbb
Santosh Chauhan <santoshchhn6@gmail.com>
2023-02-10 22:19:18 +0530

    variable

---
+++ src/variable.js
@@ -0,0 +1,10 @@
+//string
+var messages = "Hello World";
+messages.toUpperCase();
+console.log(messages);
+//number
+var amount = 20.34;
+console.log(amount);
+//boolean
+var isAdmin = false;
+console.log(isAdmin);
---
+++ src/variable.ts
@@ -0,0 +1,12 @@
+//string
+let messages: string = "Hello World";
+messages.toUpperCase();
+console.log(messages);
+
+//number
+let amount: number = 20.34;
+console.log(amount);
+
+//boolean
+let isAdmin: boolean = false;
+console.log(isAdmin);
