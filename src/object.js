f21b50b2e201ae4e54e00d04c4115479626c6697
Santosh Chauhan <santoshchhn6@gmail.com>
2023-02-11 15:44:35 +0530

    object

---
+++ src/object.js
@@ -0,0 +1,4 @@
+var user = { name: "santosh", age: 29 };
+function createUser(_a) {
+    var string = _a.name, number = _a.age;
+}
---
+++ src/object.ts
@@ -0,0 +1,6 @@
+const user = { name: "santosh", age: 29 };
+const user2 = { name: "santosh", email: "santosh@gmail.com" };
+
+function getUser(): { name: string; age: number } {
+  return user;
+}
