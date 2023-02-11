0049904e84a8b3e3e8d67f909758568065aa1e11
Santosh Chauhan <santoshchhn6@gmail.com>
2023-02-11 21:18:37 +0530

    readonly

--- src/typeAliases.ts
+++ src/typeAliases.ts
@@ -1,9 +1,25 @@
-//type
+//Example 1
 type User = {
   name: string;
   age: number;
 };
 
 function createUser(user: User) {}
-
 createUser({ name: "santosh", age: 29 });
+
+//Example 2
+type User2 = {
+  readonly _id: string; //readonly
+  name: string;
+  age: number;
+  email?: string; //optional
+};
+
+let newUser: User2 = {
+  _id: "123",
+  name: "santosh",
+  age: 29,
+};
+
+newUser.name = "suraj";
+newUser.age = 24;
