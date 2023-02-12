7176b7ef1056b1af912300f300bdfa7d846e5ca3
Santosh Chauhan <santoshchhn6@gmail.com>
2023-02-12 23:37:10 +0530

    class implements interface

--- src/class.ts
+++ src/class.ts
@@ -75,3 +75,34 @@ class Admin3 extends User9 {
     this.id = 34;
   }
 }
+
+//----------------Example 5-----------------
+//Interface Implement
+interface TakePhoto {
+  cameraMode: string;
+  filter: string;
+  burst: number;
+}
+
+interface Story {
+  createStory(): void;
+}
+class Instagram implements TakePhoto {
+  constructor(
+    public cameraMode: string,
+    public filter: string,
+    public burst: number
+  ) {}
+}
+
+class Youtube implements TakePhoto, Story {
+  constructor(
+    public cameraMode: string,
+    public filter: string,
+    public burst: number
+  ) {}
+
+  createStory(): void {
+    console.log("story created");
+  }
+}
